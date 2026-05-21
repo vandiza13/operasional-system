4806a44c77b053a50a8c87ab72ba7142009fdfe3
Mr. Robot <tb.vandiza13@gmail.com>
2026-05-21 22:21:52 +0700

    PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
